f0a6b1700335746e0ea5f758298f7575c897a6b6
Vanessa Vieira de Camargo <vanessacamargo.v@gmail.com>
2020-07-03 18:59:46 -0300

    test commands

--- Store.Domain/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
+++ Store.Domain/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -5,8 +5,33 @@ using System.Text;
 
 namespace Store.Domain.Commands.OrderCommands.Inputs
 {
-    public class PlaceOrderCommand
+    public class PlaceOrderCommand:  Notifiable, ICommand
     {
-        public Guid Customer  { get; set; }
+       public PlaceOrderCommand()
+        {
+            OrderItems = new List<OrderItemCommand>();
+        }
+
+        public Guid Customer { get; set; }
+        public IList<OrderItemCommand> OrderItems { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasLen(Customer.ToString(), 36, "Customer", "Identificador do Cliente inválido")
+                .IsGreaterThan(OrderItems.Count, 0, "Items", "Nenhum item do pedido foi encontrado")
+            );
+            return IsValid;
+        }
     }
-}
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+    }
+
+
+
+
---
+++ Store.Tests/Commands/CreateCustomerCommands.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.StoreContext.Entities;
+using Store.Domain.StoreContext.ValueObjects;
+
+namespace Store.Tests
+{
+    [TestClass]
+    public class CreateCustomerCommands
+    {
+    
+        [TestMethod]
+        public void ShouldValidateWhenCommandIsValid()
+        {
+            var command = new CreateCustomerCommands();
+            command.FirstName = "Vanessa";
+            command.LastName = "Camargo";
+            command.Document = "28659170377";
+            command.Email = "vancamargo@gmail.com";
+            command.Phone = "119999999999";
+
+            Assert.AreEqual(true, command.Valid());
+
+        }
+
+        
+    }
+}
