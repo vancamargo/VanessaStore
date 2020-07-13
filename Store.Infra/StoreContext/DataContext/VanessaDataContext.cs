b225c24fb9dddf062e75ffbd0fff238fe472d2b2
Vanessa Vieira de Camargo <vanessacamargo.v@gmail.com>
2020-07-13 20:29:37 -0300

    banco de dados feito

---
+++ Store.Infra/StoreContext/DataContext/VanessaDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Store.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Store.Infra.StoreContext.DataContext
+{
+    public class BaltaDataContext : IDisposable
+    {
+        public SqlConnection Connection { get; set; }
+
+        public BaltaDataContext()
+        {
+            Connection = new SqlConnection(Settings.ConnectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (Connection.State != ConnectionState.Closed)
+                Connection.Close();
+        }
+    }
+}
---
+++ Store.Shared/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Store.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=.\sqlexpress;Database=vanessaStore;User ID=sa;Password=teste;";
+    }
+}
